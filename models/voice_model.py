65e6a8e1dc4c910e933186314a2e7ef5529926b0
Lavina Manoj <150411309+LavinaRoshni0305@users.noreply.github.com>
2025-08-27 21:53:35 +0530

    voice_model.py

--- models/voice_model.py
+++ models/voice_model.py
@@ -1,34 +1,14 @@
-import whisper 
-import queue
-import sounddevice as sd
-import numpy as np
+import whisper
 
-model = whisper.load_model("base")
+model = whisper.load_model("base")  # or "small", "medium", etc.
+
 trigger_phrases = ["help me luna", "sos", "emergency", "save me"]
 
-def record_audio(q, duration=5, samplerate=16000):
-    def callback(indata, frames, time, status):
-        q.put(indata.copy())
-    with sd.InputStream(callback=callback, channels=1, samplerate=samplerate):
-        sd.sleep(duration * 1000)
-
-def run_voice():
-    print("[VOICE MODEL] Running...")
-    q = queue.Queue()
-    record_audio(q)
-    audio_data = []
-
-    while not q.empty():
-        audio_data.extend(q.get())
-
-    audio_np = np.array(audio_data).flatten()
-    audio = whisper.pad_or_trim(audio_np)
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    result = model.transcribe(audio_np, language='en')
+def run_voice(file_path):
+    result = model.transcribe(file_path)
     text = result['text'].lower()
 
-    print("[VOICE MODEL] Recognized:", text)
+    print("[VOICE MODEL] Transcribed:", text)
     if any(phrase in text for phrase in trigger_phrases):
         print("🚨 Emergency Triggered via Voice Command!")
         return "voice_detected"
